c9e336f5f61cc24875e386dbf04230d88cd7c0f5
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-09-08 00:29:22 +0100

    Home Page

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const button = document.querySelector('button')
 const introEl = document.querySelector('.ready')
 const formEl = document.querySelector('form')
 const navigationEl = document.querySelector('.btn')
+const HomeInput = document.querySelector('.HomeInput')
+
+// var playerName = "";
 
 button.addEventListener(
     'click',
     (e) => {
-        e.preventDefault()
+        e.preventDefault();
         introEl.style.display = "none"
         formEl.style.display = 'block'
         navigationEl.style.display = 'flex'
+    }
+
+    // if(HomeInput.value === ''){
+
+    // }
+)
+
+navigationEl.addEventListener(
+    'click',
+    (e) => {
+        e.preventDefault();
+        if(HomeInput.value === ''){
+            alert("Input Player name to continue")
+        }else if(HomeInput.value === null){
+            alert("Input Player name to continue")
+        }else{
+            var playerName = HomeInput.value
+            window.location.href = '/selectionPage.html'
+            console.log(playerName)
+        }
     }
 )
