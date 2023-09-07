691253a39f35e78da7875c668c4b8c27db4f8eb6
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-09-08 00:09:44 +0100

    Home Page

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-"use strict"
-console.log("Halo mi Amante");
-let weight = "i told them";
-console.log(weight);
+const button = document.querySelector('button')
+const introEl = document.querySelector('.ready')
+const formEl = document.querySelector('form')
+const navigationEl = document.querySelector('.btn')
+
+button.addEventListener(
+    'click',
+    (e) => {
+        e.preventDefault()
+        introEl.style.display = "none"
+        formEl.style.display = 'block'
+        navigationEl.style.display = 'flex'
+    }
+)
