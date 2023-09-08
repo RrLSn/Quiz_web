0c5059e1847b14dcdd45a50fd02b125e91707baa
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-09-08 20:19:41 +0100

    Quiz app

---
+++ script.js
@@ -0,0 +1,16 @@
+// //selector
+// const navigateEl = document.getElementsByClassName('btn')
+// const inputNameEl = document.getElementsByClassName('HomeInput')
+
+
+// navigateEl.addEventListener(
+//     'click',
+//     (e)=>{
+//         e.preventDefault()
+//         if(inputNameEl.value === ''){
+//             alert("Input Player Name to proceed")
+//         }else{
+//             window.location.href='/selectionPage.html'
+//         }
+//     }
+// )
