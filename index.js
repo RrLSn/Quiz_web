a0b4d2179dc3bbef132020b727cca669edffb5d2
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-09-08 02:12:38 +0100

    Home Page

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-const button = document.querySelector('button')
-const introEl = document.querySelector('.ready')
-const formEl = document.querySelector('form')
-const navigationEl = document.querySelector('.btn')
-const HomeInput = document.querySelector('.HomeInput')
+document.addEventListener("DOMContentLoaded", function(){
+    //Selector
+    const button = document.querySelector('.pageButton')
+    const introEl = document.querySelector('.ready')
+    const formEl = document.querySelector('form')
+    const navigationEl = document.querySelector('.btn')
+    const HomeInput = document.querySelector('.HomeInput')
+    const Selections = document.querySelector('.SelContainer')
 
-// var playerName = "";
 
-button.addEventListener(
-    'click',
-    (e) => {
-        e.preventDefault();
-        introEl.style.display = "none"
-        formEl.style.display = 'block'
-        navigationEl.style.display = 'flex'
-    }
+    //Initialization
+    const playerName = "";
 
-    // if(HomeInput.value === ''){
 
-    // }
-)
-
-navigationEl.addEventListener(
-    'click',
-    (e) => {
-        e.preventDefault();
-        if(HomeInput.value === ''){
-            alert("Input Player name to continue")
-        }else if(HomeInput.value === null){
-            alert("Input Player name to continue")
-        }else{
-            var playerName = HomeInput.value
-            window.location.href = '/selectionPage.html'
-            console.log(playerName)
+    button.addEventListener(
+        'click',
+        () => {
+            introEl.style.display = "none"
+            formEl.style.display = 'block'
+            navigationEl.style.display = 'flex'
         }
-    }
-)
+    )
+
+    navigationEl.addEventListener(
+        'click',
+        () => {
+            if(HomeInput.value === ''){
+                alert("Input Player name to continue")
+            }else{
+                playerName = HomeInput.value
+                window.location.href = '/selectionPage.html'
+            }
+        }
+    )
+})
